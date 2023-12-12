12311407253deb98754f4276db79f19036d4b4ba
mashfikur <120697837+mashfikur@users.noreply.github.com>
2023-12-13 02:37:56 +0600

    dynamic product data loaded from database

---
+++ src/components/ProductCard/ProductCard.jsx
@@ -0,0 +1,16 @@
+
+import PropTypes from 'prop-types'
+
+const ProductCard = ({product}) => {
+  return (
+    <div>
+        <h3>Product card</h3>
+    </div>
+  )
+}
+
+ProductCard.propTypes = {
+    product:PropTypes.object
+}
+
+export default ProductCard
--- src/components/Sections/ValueDeals.jsx
+++ src/components/Sections/ValueDeals.jsx
@@ -1,16 +1,35 @@
 import { useQuery } from "@tanstack/react-query";
 import useCustomAxios from "../../hooks/useCustomAxios";
+import ProductCard from "../ProductCard/ProductCard";
 
 const ValueDeals = () => {
   const axiosCustom = useCustomAxios();
 
-  const { data } = useQuery({
-    queryKey: [""],
+  const { data, isPending } = useQuery({
+    queryKey: ["value_deals"],
+    queryFn: async () => {
+      const res = await axiosCustom.get("/api/v1/products/section/value_deals");
+
+      return res.data;
+    },
   });
 
   return (
     <div>
       <h3 className="font-semibold text-4xl">Vlaue Deals</h3>
+
+      <div>
+        {isPending ? (
+          "loading"
+        ) : (
+          <div>
+            {data &&
+              data.map((product, idx) => (
+                <ProductCard key={idx} product={product}></ProductCard>
+              ))}
+          </div>
+        )}
+      </div>
     </div>
   );
 };
